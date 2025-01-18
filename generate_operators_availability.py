e1802da42e576902a3a42e7af830de04927d611a
Martin Jessenne <martin.jessenne@etu.minesparis.psl.eu>
2025-01-18 13:24:20 +0100

    Main file is working on its own, circular dependencies issues solved

--- generate_operators_availability.py
+++ generate_operators_availability.py
@@ -1,19 +1,27 @@
-from ObjectOrientedProductionSimulation import *
+import pandas as pd
+import numpy as np
+import json
+from datetime import date, datetime, timedelta
+from intervaltree import Interval, IntervalTree
+from common import *
 
 # Extract the holidays, and daily shift from the data
 dict_int_to_day = {"0": "Monday", "1": "Tuesday", "2": "Wednesday", "3": "Thursday", "4": "Friday"}
 
-def generate_operators_availability(date) -> None:
-    
-    whole_day = Interval(date, date + timedelta(days=1))    #TODO: big issue with whole_day, need to bound it to the working hours of the laboratory
-    for operator in operators:
+def generate_operators_availability(date: datetime) -> None:
+# Let's try to generate the availability of the operator day wise
+    weekday: str = str(date.weekday())
 
-        if operator.holidays.overlaps(whole_day) or date.weekday() in {5,6}:  # Check if the day is either in the holidays or weekend
-            continue  
+    for operator in operators:
+    # Reset the availability of the operator first
+        operator.availability = IntervalTree()
+
+        if len(operator.holidays[date]) or date.weekday() in {5,6}:  # Check if the day is either in the holidays or weekend
+            continue
 
         else:
-        
-            today_shift = data["OperatorWorkHours"][operator.name][dict_int_to_day[str(date.weekday())]]
+
+            today_shift = data["OperatorWorkHours"][operator.name][dict_int_to_day[weekday]]
             list_today_shift = list(today_shift.keys())
 
             # TODO: check for even number of time constraints
@@ -27,4 +35,43 @@ def generate_operators_availability(date) -> None:
                 end_time = datetime.fromisoformat(end_time)
                 operator.availability.add(Interval(start_time, end_time, "idle"))
 
+
+def is_task_assignable(tree: IntervalTree, task_duration: Interval) -> bool:    #TODO: Manually implement IntervalTree.py and add this as a method of IntervalTree.
+    assignable: bool = False
+    for interval in tree:
+        if interval.begin <= task_duration.begin and task_duration.end <= interval.end:
+            assignable = True
+            break
+    return assignable
+
+
+def generate_lab_hours(date: datetime) -> IntervalTree:
+    lab_hours: IntervalTree = IntervalTree()
+    generate_operators_availability(date)
+    for operator in operators:
+        lab_hours |= operator.availability
+    return lab_hours
+
+
+def get_next_available_time_for_task(time: datetime, task: Step) -> datetime:
+    task_duration: Interval = Interval(time, time + task.required, task.name)
+    operators_available: list = [operator for operator in operators if is_task_assignable(operator.availability, task_duration) and task.name in operator.skills]
+
+    new_time: datetime = time
+    while len(operators_available) < 2:
+        lab_hours = generate_lab_hours(new_time)
+        has_moved: bool = False
+        for interval in sorted(lab_hours):
+            if interval.begin > time:
+                time = interval.begin
+                has_moved = True
+                break
+
+        if has_moved == False:
+            new_time += timedelta(days=1)
+        else:
+            task_duration: Interval = Interval(time, time + task.required, task.name)
+            operators_available: list = [operator for operator in operators if is_task_assignable(operator.availability, task_duration) and task.name in operator.skills]
+
+    return time
 
